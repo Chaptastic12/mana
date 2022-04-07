029155cc40429adb4b531eef25cc072c956bad99
Chaptastic12 <bchapman51@gmail.com>
2022-04-06 19:07:32 -0500

    Update to ticketCard
    Users can now access ticketDetails by clicking on a ticketCard from the dashboard

--- src/DummyData.tsx
+++ src/DummyData.tsx
@@ -67,7 +67,7 @@ export let ALLPROJECTS = [
         tickets: [
             {
                 id: '3',
-                projectReference: 'MANA2',
+                projectReference: 'MANA2-1',
                 title: 'Mana 2 - Project Tracker App',
                 description: 'App used for project tracking purposes 2',
                 comments: [],
@@ -100,7 +100,7 @@ export let ALLPROJECTS = [
 export let TICKETS: Ticket[] = [
     {
         id: '3',
-        projectReference: 'MANA 2',
+        projectReference: 'MANA2-1',
         title: 'Mana 2 - Project Tracker App',
         description: 'App used for project tracking purposes 2',
         comments: [],
@@ -173,81 +173,6 @@ export let TICKETS: Ticket[] = [
             isRegUser: false
         }
     },
-    {
-        id: '6',
-        projectReference: 'MANA 2',
-        title: 'Mana 2 - Project Tracker App',
-        description: 'App used for project tracking purposes 2',
-        comments: [],
-        status: 'Finished',
-        createdDate: 'Today',
-        ticketOwner: {
-            id: '1',
-            username: 'Chaps',
-            email: 'chaps@test.com',
-            isAdmin: true,
-            isGuest: false,
-            isRegUser: false
-        },
-        ticketCreator: {
-            id: '1',
-            username: 'Chaps',
-            email: 'chaps@test.com',
-            isAdmin: true,
-            isGuest: false,
-            isRegUser: false
-        }
-    },
-    {
-        id: '4',
-        projectReference: 'MANA-1',
-        title: 'Mana - Project Tracker App',
-        description: 'App used for project tracking purposes',
-        comments: [],
-        status: 'Open',
-        createdDate: 'Today',
-        ticketOwner: {
-            id: '1',
-            username: 'Chaps',
-            email: 'chaps@test.com',
-            isAdmin: true,
-            isGuest: false,
-            isRegUser: false
-        },
-        ticketCreator: {
-            id: '1',
-            username: 'Chaps',
-            email: 'chaps@test.com',
-            isAdmin: true,
-            isGuest: false,
-            isRegUser: false
-        }
-    },
-    {
-        id: '5',
-        projectReference: 'MANA-2',
-        title: 'Test columns',
-        description: 'App used for project tracking purposes',
-        comments: [],
-        status: 'Finished',
-        createdDate: 'Today',
-        ticketOwner: {
-            id: '1',
-            username: 'Chaps',
-            email: 'chaps@test.com',
-            isAdmin: true,
-            isGuest: false,
-            isRegUser: false
-        },
-        ticketCreator: {
-            id: '1',
-            username: 'Chaps',
-            email: 'chaps@test.com',
-            isAdmin: true,
-            isGuest: false,
-            isRegUser: false
-        }
-    }
 ]
 
 export let USERS = [
--- src/pages/ProjectDashboard/TicketCard/TicketCard.tsx
+++ src/pages/ProjectDashboard/TicketCard/TicketCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Ticket } from '../../../models/models'
 import { Draggable } from 'react-beautiful-dnd';
+import { useNavigate } from 'react-router-dom';
 
 import './TicketCard.css';
 
@@ -10,11 +11,13 @@ interface Props {
 }
 
 const TicketCard = ( { index, ticketData } : Props) => {
+  const navigate = useNavigate();
+
   return (
     <Draggable draggableId={ticketData.id.toString()} index={index}>
     {
         (provided, snapshot) => (
-            <div className='TicketCard' {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} onClick={() => alert('test')}>
+            <div className='TicketCard' {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} onClick={() => navigate('/ticket/' + ticketData.projectReference)}>
                 <p> <em>{ ticketData.projectReference } </em> </p>
                 <b>{ ticketData.title }</b>
                 <p>{ ticketData.ticketOwner.username }</p>
