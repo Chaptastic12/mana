52a637d8dc7fb974056d9ba5198227f513af498a
Chaptastic12 <bchapman51@gmail.com>
2022-04-18 13:44:02 -0500

    Functions set up in react for auth
    Functions for logging in, registering and logging out have been created in the user context.
    
    LoginPage has also been created. If a user is not logged in, all theyll see is this page until they login or register

--- client/src/Context/User-Context.tsx
+++ client/src/Context/User-Context.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from 'react';
+import Axios, { AxiosResponse } from 'axios';
 
 const UserContext = createContext({});
 
@@ -9,10 +10,46 @@ export interface Props {
 const UserProvider = (props: Props) =>{
 
     const [ dashBoardTicketView, setDashBoardTicketView ] = useState<string>('horizontal');
+    const [ user, setUser ] = useState();
+
+    const loginUser = (username: string, password: string) => {
+        Axios.get('http://localhost:8081/api/auth/loginUser', {
+            params: {
+                username: username,
+                password: password
+            }
+        })
+        .then( (res: AxiosResponse) => {
+            setUser(res.data);
+            return { msg: 'Welcome, ' + res.data.username }
+        })
+    }
+
+    const registerUser = (username: string, email: string, password: string) => {
+        Axios.post('http://localhost:8081/api/auth/registerUser', {
+            params: {
+                username: username,
+                email: email,
+                password: password
+            }
+        })
+        .then( (res: AxiosResponse) => {
+            return { msg: 'User registered successfully' }
+        })
+    }
+
+    const logoutUser = () => {
+        Axios.get('http://localhost:8081/api/auth/logoutUser')
+        .then( (res: AxiosResponse) => {
+            return { msg: 'Successfully logged out' }
+        })
+    }
 
 
     return <UserContext.Provider value={{
-        dashBoardTicketView, setDashBoardTicketView
+        dashBoardTicketView, setDashBoardTicketView,
+        loginUser, registerUser, logoutUser,
+        user
     }}>
         { props.children }
     </UserContext.Provider>
